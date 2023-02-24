019ac023fe6ba96dad0a1ebc2206d865b5bd640f
techproed <burcupiaf@gmail.com>
2023-02-24 13:09:35 -0600

    v3

--- src/main/java/GitGitHub.java
+++ src/main/java/GitGitHub.java
@@ -2,5 +2,7 @@ public class GitGitHub {
 
     public static void main(String[] args) {
         System.out.println("V1 komutlari");
+        System.out.println("v2 komutlari");
+        System.out.println("v3 komutlari");
     }
 }
